41a04dd98b96190ff0f1ce48c68f7ab64414be81
ishwarg <67804752+ishwarg@users.noreply.github.com>
2021-11-07 14:58:40 -0800

    Revert "functions file now contains code from ish's_playground"
    This reverts commit b57b9dfd204e309c5fe70fd23229859dc43c6659.

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import random
 import numpy as np
+import orhelper
+generation_size=1000
 
 def generate_chromosome():
     random.seed(version=2)
@@ -59,57 +61,114 @@ def create_generation(size):
 
 import numpy as np
 
-total_gen = 15
+
+# Combos is an 1 by 1000 array of the 1000 fin cominations
 
 
-def generation_function_whateverItIs(generate_more):
-    i = 0
-    arr = np.empty((0, 3), float)
-    while (i < generate_more):
-        a = np.random.uniform(0, 5)
-        b = np.random.uniform(0, 5)
-        c = np.random.uniform(0, 5)
-        temp = np.array([[a, b, c]])
-        arr = np.append(arr, temp, axis = 0)
-        i +=1
-    return arr
+# Will get the predicted stabilities from OR -> predict all stabilities for all combinations at the same time, 
+# ... for both the full rocket and 2nd stage
+# So, pred_sb is a 2 by 1000 array of predicted stabilities
 
 def calculate_stability(combos):
-    i = 0
-    pred_sb1 = np.array([])
-    pred_sb2 = np.array([])
-    while (i < len(combos)):
-        pred_sb1 = np.append(pred_sb1, (combos[i][0]+combos[i][1]+combos[i][2]))
-        pred_sb2 = np.append(pred_sb2, combos[i][0]+combos[i][1]+0)
-        i+=1
-    pred_sb = np.array([pred_sb1, pred_sb2])
+    pred_sb = np.zeros((2,1000)) # temporary
     return pred_sb
 
 def toss_and_more(pred_sb, valid_combos, combos):
-    # avg stab is 7.5 and 5 for the two stages
-    sb_min_full = 9 # Minimum stability full rocket
-    sb_min_stage2 = 4 # Minimum stability 2nd stage
-    i = 0
-    filter_arr = (pred_sb[0] > sb_min_full)*(pred_sb[1] > sb_min_stage2)
-    filters = combos[filter_arr]
-    valid_combos = np.append(valid_combos, filters, axis = 0)
-    generate_more = total_gen - np.size(valid_combos[:, 0])
+    sb_min_full = 1.6 # Minimum stability full rocket
+    sb_min_stage2 = 1.4 # Minimum stability 2nd stage
+    valid_combos = valid_combos.append(combos[(pred_sb[0] > sb_min_full) and (pred_sb[1] > sb_min_stage2)])
+    generate_more = 1000 - np.size(valid_combos)
     return valid_combos, generate_more 
+    # Returns an array 1 by (less or equal to 1000), 
+    # and returns the number of new combos that need to be generated due to being tossed out
+    # generate_more will be fed into the generating function to create more combos ... 
+    # those combos will then be analyzed for this entire section again to approve validity and calculate cost
 
-def generate(valid_combos, generate_more):
+# helper function
+def generate(generate_more):
     combos = generation_function_whateverItIs(generate_more)
     pred_sb = calculate_stability(combos)
-    valid_combos, generate_more = toss_and_more(pred_sb, valid_combos, combos)
+    valid_combos, generate_more = toss_and_more(pred_sb)
     return valid_combos, generate_more
 
-def valid_total_gen():
-    generate_more = total_gen
-    valid_combos = np.empty((0,3), float) # Empty array
-    valid_combos, generate_more = generate(valid_combos, generate_more)
+# combination final function 
+def valid_1000():
+    generate_more = 1000
+    valid_combos = np.zeros(()) # Empty array
+    valid_combos, generate_more = generate(generate_more)
     while (generate_more > 0):
-        valid_combos, generate_more = generate(valid_combos, generate_more)
-        #print(np.size(valid_combos[:, 0]), generate_more)
+        valid_combos, generate_more = generate(generate_more)
     return valid_combos
 
-valid_combos_full = valid_total_gen()
-print(valid_combos_full)
+valid_combos_full = valid_1000()
+
+
+# This next step of evaluating cost functions will not be done until we have 1000 valid combinations
+
+
+# Will have to get the predicted height from simulation -> predict all heights for all combinations at the same time
+# So, pred_h is a 1 by 1000 array of predicted heights
+def calculate_height(valid_combos_full):
+    pred_h = np.zeros((1000)) # temporary
+    return pred_h
+
+def calculate_cost(pred_h):
+    return (30,500 - pred_h)**2
+
+
+def choose_parents(all_parents, all_costs):
+
+   worst=max(all_costs)
+   all_costs[np.argmin(all_costs)]+=1
+   likelihood=1/(all_costs/worst)
+   return likelihood
+
+
+    
+
+def crossover(sorted_combos, index_to_consider, percent_to_consider):
+
+
+    amount=(int(percent_to_consider*len(sorted_combos))-index_to_consider)
+    if amount-index_to_consider % 2 !=0:
+        amount-=1
+    prelim_parents=sorted_combos[index_to_consider:amount]
+    parents=choose_parents(prelim_parents, )
+
+
+    children=[None]*len(sorted_combos)
+
+    count=0
+    while count<len(parents):
+        beta=random.random()
+        crossover_point=random.range(0,6)
+
+        child1=convert_string_to_array(parents[random.randrage(index_to_consider,amount)])
+        child2=convert_string_to_array(parents[random.randrage(index_to_consider,amount)])
+
+        child1[crossover_point]=(1-beta)*child1[crossover_point]+beta*child2[crossover_point]
+        child2[crossover_point]=(1-beta)*child2[crossover_point]+beta*child1[crossover_point]
+
+        children[count]=convert_to_continuous_string(child1)
+        children[count+1]=convert_to_continuous_string(child2)
+        
+        count+=2
+    
+    while count<len(children):
+        
+        count+=2
+
+    return children
+
+
+
+    
+
+
+# run
+pred_h = calculate_height()
+costs = calculate_cost(pred_h)
+
+
+
+
