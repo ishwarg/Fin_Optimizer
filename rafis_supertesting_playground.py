aa91b118045cbfcb948f774853de26a692829339
RHAKIM9374 <rhakim9374@gmail.com>
2022-01-16 14:52:27 -0800

    rafi's testing playground has been updated with the initial tests for fin flutter and divergence, all done in Murican Eagle Units

--- rafis_supertesting_playground.py
+++ rafis_supertesting_playground.py
@@ -1,20 +1,26 @@
 import numpy as np
 
+
+# Murican Eagle Units:
+# Presure in Psi
+# Speeds in mph
+# Lengths in inches
+
 # inputed
-G = "shear modulus" # may differ in directions, use minimum value with safety factor 
-t = "fin thickness"
+G = 1667934 #"shear modulus" # may differ in directions, use minimum value with safety factor 
+t = .118 #"fin thickness"
 
 # taken from open rocket at maximum velocity
-a = "speed of sound @ sim temp height"
-P = "atmospheric presure @ sim temp height"
+a = 720.23 #"speed of sound @ sim temp height" -> taken to be m/s
+P = 10.57 #"atmospheric presure @ sim temp height"
 
-root_cord = 10
-tip_cord = 15
-S = "surface area of fin"
-b = "semispan"
+root_chord = 11.811
+tip_chord = 0.394
+b = 6.3 #"semispan"
+S = 0.5*(root_chord+tip_chord)*b #"surface area of fin"
 
 # natural
-P0 = "atmospheric presure @ sea level temp height"
+P0 = 14.7 #"atmospheric presure @ sea level temp height"
 
 def get_flutter(root_chord, tip_chord, G, S, t, b, a, P, P0):
   lam = tip_chord/root_chord
@@ -25,5 +31,8 @@ def get_flutter(root_chord, tip_chord, G, S, t, b, a, P, P0):
 
 def get_divergence(root_chord, tip_chord, G, S, t, b, a, P, P0):
   AR = (b**2)/S
-  calc = (3.3*P)/(1+(2/AR)) * ((root_chord+tip_chord)/(t**3)) * b**2 
+  calc = (3.3*P)/(1+(2/AR)) * ((root_chord+tip_chord)/(t**3)) * b**2
   return a * np.sqrt(G / calc)
+
+print(get_flutter(root_chord, tip_chord, G, S, t, b, a, P, P0))
+print(get_divergence(root_chord, tip_chord, G, S, t, b, a, P, P0))
